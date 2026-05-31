fec144fb565f231a76c6c665cf47490dfc8abe99
litdebbie <sarah.nk.ngo@gmail.com>
2026-05-30 23:13:55 -0500

    Set up an animation loop and added a background

--- index.js
+++ index.js
@@ -11,15 +11,34 @@ window.onload = function() {
     canvas.width = GameViewport.WIDTH;
     canvas.height = GameViewport.HEIGHT;
 
-    const hiei = document.querySelector('img');
+    const [hiei, background] = document.querySelectorAll('img');
 
-    c.strokeStyle = 'yellow';
-    c.moveTo(0, 0);
-    c.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-    c.moveTo(GameViewport.WIDTH, 0);
-    c.lineTo(0, GameViewport.HEIGHT);
-    c.stroke();
+    const position = {
+        x: GameViewport.WIDTH / 2 - hiei.width / 2,
+        y: 130,
+    };
 
-    c.drawImage(hiei, 0, 0);
-    console.log(c);
+    let velocity = 1;
+
+    function frame() {
+        position.x += velocity;    // update character's x position
+
+        if(position.x > GameViewport.WIDTH - hiei.width || position.x < 0) velocity = -velocity;     // ensure character does not go off screen
+
+        // c.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);     // clear canvas
+        c.drawImage(background, 0, 0);
+
+        // c.strokeStyle = 'yellow';
+        // c.moveTo(0, 0);
+        // c.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+        // c.moveTo(GameViewport.WIDTH, 0);
+        // c.lineTo(0, GameViewport.HEIGHT);
+        // c.stroke();
+
+        c.drawImage(hiei, position.x, position.y);    // draw character on screen
+        window.requestAnimationFrame(frame);    // "animate" character
+    }
+
+    window.requestAnimationFrame(frame);
+    // console.log(c);
 }
